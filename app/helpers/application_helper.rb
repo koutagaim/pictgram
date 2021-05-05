049a23d2f564bc5eb0e8131d714e09fcb0b4da3b
koutagaim <ryuku.kouta@gmail.com>
2021-05-05 09:27:54 +0000

    “04課題再提出”

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,3 @@
 module ApplicationHelper
-    helper_method :current_user, :logged_in?
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
-
-  def logged_in?
-    !current_user.nil?
-  end
+   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-    validates :name, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, length: { maximum: 15}
-    validates :email, presence: true
-    
-    has_secure_password
     VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{8,32}+\z/
-validates :password, presence: true, length: { minimum: 8, maximum: 32}, format: { with: VALID_PASSWORD_REGEX}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+    validates :name, presence: true, length: { maximum: 15}
+    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}
+    validates :password, presence: true, length: { minimum: 8, maximum: 32}, format: { with: VALID_PASSWORD_REGEX}
+
+    has_secure_password
 end
